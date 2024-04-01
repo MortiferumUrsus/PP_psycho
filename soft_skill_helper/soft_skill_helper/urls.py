8550d0aab0c296eca9cced13bd136274716c1653
Mikhail Sadovskiy <misha.sadovskiy@yandex.ru>
2024-04-01 22:03:34 +0300

    Добавил главную страничку

--- soft_skill_helper/soft_skill_helper/urls.py
+++ soft_skill_helper/soft_skill_helper/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from greatpage.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('greatpage/', index), #127.0.0.1.8000/greatpage/
 ]
